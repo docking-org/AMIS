ca5cf1843344c272f84600a00cabd091ee66f3e5
Chinzo Dandar <chinzo@velvet.desktop.ucsf.bkslab.org>
2018-12-05 13:45:26 -0800

    deleted unnessesary configs

--- config.py
+++ config.py
@@ -20,22 +20,5 @@ class Config(object):
 
     # Flask-User settings
     USER_APP_NAME = "A Mouse Image Server"  # Shown in and email templates and page footers
-    USER_ENABLE_EMAIL = True  # Enable email authentication
-    USER_ENABLE_USERNAME = True  # Register and Login with username
-    USER_LOGIN_TEMPLATE = 'flask_user/login_or_register.html'
-    USER_REGISTER_TEMPLATE = 'flask_user/login_or_register.html'
-    USER_LOGOUT_URL = '/user/logout'
-    USER_LOGIN_URL = '/user/login'
-    USER_ENABLE_FORGOT_PASSWORD = True
-    USER_AFTER_LOGIN_ENDPOINT = 'main.index'
-    USER_EMAIL_SENDER_NAME = USER_APP_NAME
-    USER_EMAIL_SENDER_EMAIL = os.getenv("EMAIL")
 
     CSRF_ENABLED = True
-
-    SCRIPT_SOURCE = os.getenv("SCRIPT_SOURCE")
-
-    S3_BUCKET = os.getenv("S3_BUCKET") or 'S3_BUCKET'
-    S3_KEY = os.getenv("S3_KEY") or 'S3_KEY'
-    S3_SECRET = os.getenv("S3_SECRET") or 'S3_SECRET'
-    S3_LOCATION = os.getenv("S3_LOCATION") or 'S3_LOCATION'
