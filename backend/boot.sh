b602d176e4848ed99b70a412028ab38343d13328
mar <josecastanon4@gmail.com>
2024-07-01 10:06:50 -0700

    adding more workers

--- backend/boot.sh
+++ backend/boot.sh
@@ -2,6 +2,6 @@
   
 set -m
 
-gunicorn  -b :5000 -w 8 --log-level=DEBUG --access-logfile - --error-logfile - application:application  --timeout 36000 --reload &
+gunicorn  -b :5000 -w 16 --log-level=DEBUG --access-logfile - --error-logfile - application:application  --timeout 36000 --reload &
   
 fg %1
